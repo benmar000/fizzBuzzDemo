e93f2593ee5454f281d075adefe5b0b01e5b7837
Ben Martinez <ben.martinez@clincierge.com>
2022-07-07 12:54:57 -0400

    Refactor fizzBuzz to remove else statements

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,13 +4,13 @@ const fizzBuzz = () => {
   let i = 1;
   while (i <= 100) {
     let result = "";
-    if (isDivisible(i, 3) && isDivisible(i, 5)) {
-      result = "FizzBuzz";
-    } else if (isDivisible(i, 3)) {
+    if (isDivisible(i, 3)) {
       result = "Fizz";
-    } else if (isDivisible(i, 5)) {
-      result = "Buzz";
-    } else {
+    }
+    if (isDivisible(i, 5)) {
+      result += "Buzz";
+    }
+    if (result === "") {
       result = i;
     }
     console.log(result);
